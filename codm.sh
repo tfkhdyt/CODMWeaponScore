243def2839126bae50354aeb79bfd76c7702a854
tfkhdyt <ta142fa@gmail.com>
2021-01-24 20:48:21 +0700

    Menambah data range dan mengubah metode kalkulasi

--- codm.sh
+++ codm.sh
@@ -6,20 +6,22 @@ ws(){
   fireRate=$3
   accuracy=$4
   mobility=$5
-  control=$6
+  range=$6
+  control=$7
 
   #proses
-  DPS=$(( $2 * $3 / 100  ))
-  acCMo=$(( ( $4 + $6 ) / 2 * $5 / 100 ))
+  DPS=$(( $2 * $3 / 2 / 100  ))
+  acCMo=$(( ( $4 + $7 ) / 2 * $5 * $6 / 3 / 100 ))
   
   #output
+  echo "-------------------------------------"
   echo "Nama Senjata : $1"
   echo -e "\nDamage       : $2 | Firerate     : $3"
   echo "Accuracy     : $4 | Mobility     : $5"
-  echo "Control      : $6"
+  echo "Range        : $6 | Control      : $7"
   
-  echo -e "\nDPS          : $DPS | Acc/Cont/Mob : $acCMo"
+  echo -e "\nDPS          : $DP| Ac/Mo/Ra/Co  : $acCMo"
 
   echo -e "\nTotal score  : $(( ( $DPS + $acCMo ) / 2 ))"
-  echo "-------------------------------"
+  echo "-------------------------------------"
 }
